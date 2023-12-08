e3bdd507fdb00b4428b5e96ad4995c4b9708f5c0
Altamir Rodrigues <box.altamir@gmail.com>
2023-12-08 14:45:04 +0000

    Projeto inicial da Faculade em C

---
+++ projetofaculdade.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//Definição da estrutura
+
+typedef struct 
+{
+    /* data */
+}Estudante;
+
+typedef struct 
+{
+   
+    /* data */
+}Uc;
+
+typedef struct 
+{
+    /* data */
+}Avaliacao;
+
+
+ 
